667c9734d5577758ddaf38d094da18c9e4fd2eda
wwwzhangshenzecn <2511695680@qq.com>
2018-03-05 22:30:39 +0800

    Update __init__.py

--- music_csv/__init__.py
+++ music_csv/__init__.py
@@ -1,8 +1,18 @@
 from  music_csv import *
 import  os
+
+'''
+将本文件夹中的所有音乐文件扫描，输出为csv格式列表
+
+'''
 if __name__=='__main__':
     path=os.path.abspath(os.path.dirname(__file__))
     print(path)
     row=[]
     getMusicinfo(path,row)
     write(row)
+
+    while(1):
+        temp =input("输入exit退出：")
+        if(temp == 'exit'):
+            exit(0)
