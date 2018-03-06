210adeecdaf56edb31c94ec3622f7fa3c13a965b
wwwzhangshenzecn <2511695680@qq.com>
2018-03-06 19:24:22 +0800

    Update music_csv.py

--- music_csv/music_csv.py
+++ music_csv/music_csv.py
@@ -2,19 +2,46 @@ import os
 import os.path
 import time
 import csv
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+from mutagen import File
 
+def getID3(str_filename,pathTemp):
+    audiofile = MP3(pathTemp, ID3=EasyID3)
 
-def getID3(str_filename):
-    index = str_filename.find('-')
-    if index <= 1:  # 防止出现无名称文件夹
-        index = 2
-    author = str_filename[:index - 1]  # 提取MP3歌手
-    name = str_filename[index+2:-4] #歌曲名
-    t =time.strftime('%Y-%m-%d %X', time.localtime(time.time()))
+    try:
+        Album =audiofile['album'][0]
+    except:Album=""
+    try:
+        Title =audiofile['title'][0]
+    except:Title=str_filename
+    try:
+        Artist =audiofile['artist'][0]
+    except:Artist=""
+    try:
+        Albumartist =audiofile['albumartist'][0]
+    except:Albumartist=""
+    try:
+        Date =audiofile['date'][0]
+    except:
+        Date=""
+    t =time.strftime('%Y-%m-%d %X', time.localtime(time.time())) #添加时间
     path =str(os.path.abspath(str_filename))
     path=path.replace('\\','\\\\')
-    print("\r"+str_filename)
-    row=(name,author,path,t)
+    # print(path)
+    try:
+        #读取封面
+        temppath =os.path.abspath(os.path.dirname(pathTemp))
+
+        afile =File(pathTemp)
+        imgfile =afile.tags['APIC:'].data
+        img =open(temppath+'\\\\'+Title+'.jpg','wb')
+        img.write(imgfile)
+        Imgpath = str(os.path.abspath(Title + '.jpg'))
+        Imgpath.replace('\\','\\\\')
+    except: Imgpath=""
+
+    row=(Title,Artist,Album,Albumartist,Date,path,Imgpath,t)
     return row
 
 def getMusicinfo(path,row):
@@ -35,7 +62,7 @@ def getMusicinfo(path,row):
         if os.path.isdir(pathTemp):
            getMusicinfo(pathTemp,row)
         elif filename[-4:].lower() == '.mp3':
-            temp=getID3(str_filename)
+            temp=getID3(str_filename,pathTemp)
             row.append(temp)
         else: pass
 
@@ -45,9 +72,12 @@ def write(row):
     # print(row)
     csvfile = open('music.csv', 'w', newline="", encoding='utf-8')
     write = csv.writer(csvfile)
-    write.writerow(('Name', 'Author','Path','Time'))
+    tfile =open('music.txt','w',encoding='utf-8')
     for n in row:
         write.writerow(n)
+        for k in n:
+            tfile.write(str(k)+'\t')
+        tfile.write('\n')
         #print(n,end='\n',flush="false")
     csvfile.close()
 
